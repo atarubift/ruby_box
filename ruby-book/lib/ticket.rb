086afc1bcf30417cd51ca90fcaa01ee1deba8cfd
atarubift <atarubift@gmail.com>
2022-05-25 10:53:52 +0900

    add rainbow

--- ruby-book/lib/ticket.rb
+++ ruby-book/lib/ticket.rb
@@ -1,12 +1,11 @@
 class Ticket
+  attr_reader :fare, :stamped_at
+
   def initialize(fare)
     @fare = fare
   end
 
-  def enter(ticket)
-  end
-
-  def exit(ticket)
-    true
+  def stamp(name)
+    @stamped_at = name
   end
 end
--- ruby-book/test/gate_test.rb
+++ ruby-book/test/gate_test.rb
@@ -3,12 +3,33 @@ require_relative '../lib/gate'
 require_relative '../lib/ticket'
 
 class GateTest < Minitest::Test
-  def test_gate
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
 
+  def test_umeda_to_juso
     ticket = Ticket.new(160)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
+  end
+
+  def test_umeda_to_mikuni_when_fare_is_not_enough
+    ticket = Ticket.new(160)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
+  end
+
+  def test_umeda_to_mikuni_when_fare_is_enough
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
+
+  def test_juso_to_mikuni
+    ticket = Ticket.new(160)
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
   end
 end
